b852cff5f4cc7c2c3166ecde10e40a8c650b52cd
Vale2197 <valentinatrebbi97@gmail.com>
2021-11-09 14:29:48 +0100

    stampate e mail a schermo.. dopo qualche secondo..

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const root = new Vue({
     el: "#root",
     data: {
-
+        listaMail: [],
     },
     mounted() {
-        
+        for (let i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(risp => {
+                console.log(risp);
+                let mail = risp.data.response;
+
+                if (!this.listaMail.includes(mail)) {
+                    this.listaMail.push(mail)
+                }
+                else {
+                    i--
+                }
+            })
+            
+        }
+            console.log(this.listaMail);
+
     }
 })
